ae1848b79331c72a0082bba96398c9f21529862a
MordorWorld <gonzalo.gdelpozo@gmail.com>
2025-05-12 19:12:26 +0200

    Update AVD-SubRSI.ps1
    V 0.2

--- AVD-SubRSI.ps1
+++ AVD-SubRSI.ps1
@@ -1,2 +1,8 @@
+$FileUri = "https://raw.githubusercontent.com/MordorWorld/AzureScripts/refs/heads/main/AVD-SubRSI.ps1"
+
+
 $Keypath = "HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters"
 New-ItemProperty -Path $Keypath -Name "AllowSingleLabelDnsDomain" -Value 1
+
+New-Item -ItemType Directory -Path "C:\work"
+New-Item -Name "C:\work\ok.txt" -ItemType File
